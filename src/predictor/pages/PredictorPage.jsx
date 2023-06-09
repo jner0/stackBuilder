71164f5ad2bf692a67fa4bac66c21f2c861935d7
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-06-08 23:56:02 -0500

    Adding form

--- src/predictor/pages/PredictorPage.jsx
+++ src/predictor/pages/PredictorPage.jsx
@@ -1,9 +1,42 @@
 import { Navbar } from "../components/Navbar"
 
+import { useForm } from "react-hook-form";
+
 export const PredictorPage = () => {
+
+    const { register, errors, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
   return (
     <>
         <Navbar/>
+
+        <section className="container mt-5">
+            <form onSubmit={ handleSubmit(onSubmit) }>
+                <label className="form-label mt-2">Número de placa</label>
+                <input
+                    className="form-control my-2"
+                    {...register("placa")}
+                />
+                <label className="form-label mt-2">Fecha</label>
+                <input
+                    type="date"
+                    className="form-control my-2"
+                    {...register("fecha")}
+                />
+                <label className="form-label mt-2">Hora</label>
+                <input
+                    type="time"
+                    className="form-control my-2"
+                    {...register("hora")}
+                />
+
+                <button type="submit" className="btn btn-primary">Send</button>
+            </form>
+        </section>
     </>
   )
 }
